2228bea1c77083be85ee3edb3e28af11e62ba969
Berat Erol <146940014+BeratErol@users.noreply.github.com>
2024-11-20 12:48:27 +0300

    Add files via upload

---
+++ Ntp_Lab_5.Hafta/TekYonluOrnek2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Kitap
+{
+    public string Baslik { get; set; }
+    public string ISBN { get; set; }
+
+	public Kitap(string baslik, string isbn)
+	{
+        Baslik = baslik;
+        ISBN = isbn;
+	}
+}
+
+class Yazar
+{
+    public string Ad { get; set; }
+    public string Ulke { get; set; }
+    public List<Kitap> Kitaplar { get; set; }
+
+    public Yazar(string ad, string ülke)
+    {
+        Ad = ad;
+        Ulke = ülke;
+        Kitaplar = new List<Kitap>();
+    }
+
+    public void KitapEkle(Kitap kitap)
+    {
+        Kitaplar.Add(kitap);
+    }
+}
---
+++ Ntp_Lab_5.Hafta/ZayifBagimlilikOrnek2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Oda
+{
+    public string Boyut { get; set; }
+    public string Tip { get; set; }
+
+    public Oda(string boyut, string tip)
+    {
+        Boyut = boyut;
+        Tip = tip;
+    }
+}
+
+class Ev
+{
+    public string Ad { get; set; }
+    public List<Oda> Odalar { get; set; }
+
+    public Ev(string ad)
+    {
+        Ad = ad;
+        Odalar = new List<Oda>();
+    }
+
+    public void OdaEkle(Oda oda)
+    {
+        Odalar.Add(oda);
+    }
+}
